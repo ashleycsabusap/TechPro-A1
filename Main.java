df66c80d6581bef6fb8ea2d02915233d75b93d18
ashleycsabusap <ashleycsabusap4@gmail.com>
2025-08-29 02:09:43 +0000

    done

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main{
     public static void main(String[] args) {
-        
+
+        System.out.println("Ashley Sabusap");
+        int countdown = 3;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("Happy New Year!!");
     }
 }
