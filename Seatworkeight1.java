f0af4afd5036567568d74ab726f261b53572ac0a
ashleycsabusap <ashleycsabusap4@gmail.com>
2026-01-27 02:10:30 +0000

    Done

---
+++ Seatworkeight1.java
@@ -0,0 +1,23 @@
+class Student {
+	String name;
+	
+	void displayName() {
+		System.out.println("String Name:" + name);
+	}
+}
+
+public class Mai {
+	public static void main(String[] args) {
+		
+		Student s1 = new Student();
+		Student s2 = null;
+		s1.name = "Ashley Sabusap";
+		
+		s1.displayName();
+		try {
+			s2.displayName();
+		} catch (Exception e) {
+		System.out.println("Error: Student Object is not created");
+		}
+	}
+}
