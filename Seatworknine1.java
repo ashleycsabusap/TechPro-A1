e01fae78ed602601febf0fc17cb5809d9408e1fa
ashleycsabusap <ashleycsabusap4@gmail.com>
2026-01-27 02:24:11 +0000

    Done

---
+++ Seatworknine1.java
@@ -0,0 +1,17 @@
+public class Seatworknine1 {
+	public static void main(String[] args) {
+		
+		int number1=20;
+		int number2=0;
+		
+		try{
+			int result= number1/number2;//causes error
+			System.out.println("Result:"+ result);
+		} catch (ArithmeticException e) {
+			System.out.println("Built-in Exception: Cannot divide by zero");
+		
+	}
+	
+	System.out.println("Program continues running...");
+	}
+}
