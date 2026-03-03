c025e54536ac0086fbf4463c0774df3ac3f2b323
ashleycsabusap <ashleycsabusap4@gmail.com>
2026-03-03 12:01:06 +0000

    Done

---
+++ Assignment1.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Button Example");
+        JButton button = new JButton("Click Me");
+
+        button.setBounds(130, 100, 100, 40);
+
+        button.addActionListener(e -> 
+            JOptionPane.showMessageDialog(frame, "Button Clicked!")
+        );
+
+        frame.add(button);
+        frame.setSize(400, 300);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ Assignment3.java
@@ -0,0 +1,14 @@
+
+ import javax.swing.*;
+
+public class Assignment3 {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("My First Swing Window");
+
+        frame.setSize(400, 300);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
